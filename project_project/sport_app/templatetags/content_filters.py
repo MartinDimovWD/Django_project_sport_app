7384208707bfea7ecf82880db253cc85c1e83ee5
MartinDimovWD <75526884+MartinDimovWD@users.noreply.github.com>
2022-12-15 13:15:18 +0200

    Improvements to the Workouts list and edit template
    Add a modal window with workout details.
    There is a filter generating some random sets and reps.

--- project_project/sport_app/templatetags/content_filters.py
+++ project_project/sport_app/templatetags/content_filters.py
@@ -1,3 +1,5 @@
+import random
+
 from django.template import Library
 
 register = Library()
@@ -6,3 +8,17 @@ register = Library()
 @register.filter('capitalize_each_word')
 def capitalize_each_word(str):
     return " ".join([x.capitalize() for x in str.split()])
+
+
+@register.filter('random_reps')
+def random_reps(exercise):
+    reps = random.randint(7, 14)
+    sets = random.randint(3, 5)
+    weight = random.randint(5,40)
+    duration = random.randint(10, 40)
+    if exercise.type == 'bodyweight':
+        return f'{sets} sets X {reps} reps'
+    elif exercise.type == 'machine' or exercise.type == 'free weights':
+        return f'{sets} sets X {reps} reps X {weight} kg'
+    elif exercise.type == 'cardio':
+        return f'Duration: {duration} minutes'
