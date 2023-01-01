211bcbd57a161a9fce2f6b69f8591825f30b2504
MartinDimovWD <75526884+MartinDimovWD@users.noreply.github.com>
2023-01-01 23:07:42 +0200

    Goal Management Styles
    The goal management panel got some nice improvements. The create goal form now shows in a modal if the client wants to add a new one.
    
    To do:
    - make the container responsive.
    - add sorting options.
    - add some statistics

--- staticfiles/scripts/buttons.js
+++ staticfiles/scripts/buttons.js
@@ -2,4 +2,7 @@
 const confirmPrimeChangeBtn = document.getElementById('change-prime-confirm-btn')
 const primeCheckboxInput = document.getElementById('id_prime_membership')
 
+const goalMngCreateBtn = document.getElementById('goal-management-create-goal-btn')
+const goalMngCreateForm = document.getElementById('goal-management-create-goal-form')
 // TODO: disable buttons if no change is made to the inputs
+
